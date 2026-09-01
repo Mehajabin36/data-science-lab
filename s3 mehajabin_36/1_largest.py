f31bcf01ff869753462c83ea72c8ca41afc55743
Mehajabin36 <shabnamahin67@gmail.com>
2026-09-01 11:18:15 +0530

    Delete s3 mehajabin_36 directory

--- s3 mehajabin_36/1_largest.py
+++
@@ -1,3 +0,0 @@
-list=[10,20,30,40]
-num=max(list)
-print("largest number is:" ,num)
--- s3 mehajabin_36/2_duplicate.py
+++
@@ -1,5 +0,0 @@
-list1=[10,10,20,60,80,60]
-print("duplicated list:",list1)
-list2=list(set(list1))
-print("duplicates removed list: ",list2)
-
